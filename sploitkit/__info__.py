6e03d73a130c00a77cd24e268d42f33bbae196fc
Alex <alexandre.dhondt@gmail.com>
2019-04-29 08:00:22 +0200

    New PyPi release

--- sploitkit/__info__.py
+++ sploitkit/__info__.py
@@ -5,6 +5,6 @@
 """
 
 __author__    = "Alexandre D'Hondt"
-__version__   = "0.1.0"
+__version__   = "0.1.1"
 __copyright__ = "© 2019 A. D'Hondt"
 __license__   = "AGPLv3 (http://www.gnu.org/licenses/agpl.html)"
