43ed0a93890f397e48a88d7e654dd2186343571c
Alex <alexandre.dhondt@gmail.com>
2019-04-29 07:52:16 +0200

    First PyPi release

---
+++ sploitkit/__info__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""Sploitkit package information.
+
+"""
+
+__author__    = "Alexandre D'Hondt"
+__version__   = "0.1.0"
+__copyright__ = "© 2019 A. D'Hondt"
+__license__   = "AGPLv3 (http://www.gnu.org/licenses/agpl.html)"
