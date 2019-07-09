fa79f0e565af72c1d26f054d5e4cce135ea8451d
dhondta <alexandre.dhondt@gmail.com>
2019-07-09 14:12:55 +0200

    Improved command core module

--- sploitkit/core/command.py
+++ sploitkit/core/command.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 import gc
 import re
 from inspect import getargspec
-from six import u, with_metaclass
 
 from ..utils.meta import Entity, MetaEntity
 from ..utils.misc import failsafe
@@ -55,21 +54,21 @@ def load_commands(*sources, **kwargs):
             sources.insert(0, str(_))
     # now, it loads every Python module from the list of sources ; when loading
     #  Command subclasses, these are registered to Command.commands, organized
-    #  as a dictionary by command levels and Command._subclasses for further
+    #  as a dictionary by command levels and Command.subclasses for further
     #  direct access (i.e. from the console)
     for source in sources:
-        PyModulePath(source).get_classes(Command)
+        PyModulePath(source)
     # handle proxy classes exclusion ; this will remove classes like for example
     #  RootCommand which aims to define level="root" for related level commands
-    for c in Command._subclasses:
+    for c in Command.subclasses:
         if len(c.__subclasses__()) > 0:
             Command.unregister_command(c)
     # handle specific commands or sets of commands exclusions ; this will remove
-    #  them from the Command.commands and Command._subclasses dictionaries
+    #  them from the Command.commands and Command.subclasses dictionaries
     Command.unregister_items(*kwargs.get("exclude", []))
     # handle conditional commands ; this will remove commands having a
     #  "condition" method returning False
-    for c in Command._subclasses:
+    for c in Command.subclasses:
         c, o = c, c()
         if hasattr(o, "condition") and not o.condition():
             Command.unregister_command(c)
@@ -80,7 +79,7 @@ class MetaCommand(MetaEntity):
     """ Metaclass of a Command. """
     style = "slugified"
     
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args):
         argspec = getargspec(self.run)
         s, args, defs = "{}", argspec.args[1:], argspec.defaults
         for a in args[:len(args)-len(defs or [])]:
@@ -115,13 +114,23 @@ class MetaCommand(MetaEntity):
         return _
 
 
-class Command(with_metaclass(MetaCommand, Entity)):
+class Command(Entity, metaclass=MetaCommand):
     """ Main class handling console commands. """
     aliases = []
+    alias_only = False
     commands = {}
     level = "general"
     levels = []
+    except_levels = []
     splitargs = True
+    
+    @property
+    def _nargs(self):
+        """ Get run's signature info (n = number of args,
+                                      m = number of args with no default). """
+        argspec = getargspec(self.run)
+        n = len(argspec.args) - 1  # substract 1 for self
+        return n, n - len(argspec.defaults or ())
     
     @property
     @failsafe
@@ -151,7 +160,7 @@ class Command(with_metaclass(MetaCommand, Entity)):
             for l in levels:
                 for n, c in cls.commands.get(l, {}).items():
                     if c.level != "general" or all(l not in levels for l in \
-                                               getattr(c, "except_levels", [])):
+                                                   c.except_levels):
                         _[n] = c
             # then rebuild the dictionary by levels from this dictionary
             levels = {"general": {}, "specific": {}}
@@ -172,7 +181,7 @@ class Command(with_metaclass(MetaCommand, Entity)):
             t = BorderlessTable(d, "{} commands".format(l.capitalize()))
             s += t.table + "\n\n"
             i += 1
-        return u("\n" + s.strip() + "\n") if i > 0 else ""
+        return "\n" + s.strip() + "\n" if i > 0 else ""
 
     @classmethod
     def register_command(cls, subcls):
@@ -182,7 +191,7 @@ class Command(with_metaclass(MetaCommand, Entity)):
         cls.commands.setdefault(l, {})
         if l not in cls.levels:
             cls.levels.append(l)
-        if not getattr(subcls, "alias_only", False):
+        if not subcls.alias_only:
             cls.commands[l][subcls.name] = subcls
         for alias in subcls.aliases:
             cls.commands[l][alias] = subcls
@@ -207,14 +216,15 @@ class Command(with_metaclass(MetaCommand, Entity)):
                 pass
         # remove the subclass instance from the subclasses registry
         try:
-            Command._subclasses.remove(subcls)
+            Command.subclasses.remove(subcls)
         except ValueError:
             pass
-        # remove the subclass from the global namespace
-        try:
-            del globals()[subcls.__name__]
-        except KeyError:
-            pass
+        # remove the subclass from the global namespace (if not Command itself)
+        if subcls is not Command:
+            try:
+                del globals()[subcls.__name__]
+            except KeyError:
+                pass
         # if the level of commands is become empty, remove it
         if len(Command.commands[l]) == 0:
             del Command.commands[l]
@@ -241,25 +251,17 @@ class Command(with_metaclass(MetaCommand, Entity)):
                     c = Command.commands[l][n]
                     Command.unregister_command(c)
                 except KeyError:
-                    pass
-    
-    def __getsig(self):
-        """ Get run's signature info (n = number of args,
-                                      m = number of args with no default). """
-        argspec = getargspec(self.run)
-        n = len(argspec.args) - 1  # substract 1 for self
-        return n, n - len(argspec.defaults or ())
-        
+                    pass        
     
     def complete_options(self):
         """ Default option completion method. """
-        if self.__getsig()[0] > 1:
+        if self._nargs[0] > 0:
             return self.options or []
         return []
     
     def complete_values(self, option=None):
         """ Default value completion method. """
-        if self.__getsig()[0] > 0:
+        if self._nargs[0] > 1:
             try:
                 return self.values or []
             except:
@@ -276,7 +278,7 @@ class Command(with_metaclass(MetaCommand, Entity)):
         # then, start checking the signature and, if relevant, validating
         #  options and arguments
         n_in = len(args)
-        n, m = self.__getsig()
+        n, m = self._nargs
         if n_in < m or n_in > n:
             pargs = "from %d to %d" % (m, n) if n != m else "%d" % n
             raise TypeError("validate() takes %s positional argument%s but %d "
