7b7bf763280e9e1abc0deb33454ea44fedeceb51
dhondta <alexandre.dhondt@gmail.com>
2019-07-09 12:53:52 +0200

    Added test modules (to be removed after testing)

---
+++ sploitkit/base/modules/test/test.py
@@ -0,0 +1,13 @@
+from sploitkit import Module
+
+
+class Test(Module):
+    """ Test string 
+    
+    Author:  me
+    Version: 1.0
+    Freetext: This is a sample description with
+               newlines for testing __doc__ parsing as an attribute for
+               the Test module class.
+    """
+    path = "scanning/test"
