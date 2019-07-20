f6af31d9f4d7665ead36f02f11fd4775eac6f155
dhondta <alexandre.dhondt@gmail.com>
2019-07-20 08:55:59 +0200

    Adapted setup

--- setup.py
+++ setup.py
@@ -27,15 +27,13 @@ setup(
   long_description_content_type='text/markdown',
   keywords = ["CLI", "tool"],
   classifiers = [
-    'Development Status :: 4 - Beta',
+    'Development Status :: 3 - Alpha',
     'Environment :: Console',
     'Intended Audience :: Developers',
     'Intended Audience :: Information Technology',
     'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
     'Topic :: Security',
     'Topic :: Software Development :: Libraries :: Python Modules',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
@@ -44,5 +42,5 @@ setup(
     'Programming Language :: Python :: 3.8',
   ],
   install_requires=[str(r.req) for r in requirements],
-  python_requires = '>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,<4',
+  python_requires = '>=3.4,<4',
 )
