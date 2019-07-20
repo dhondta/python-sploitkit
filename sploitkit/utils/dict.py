533cd06142e9dc13c299b59d4a07afd28796536b
dhondta <alexandre.dhondt@gmail.com>
2019-07-20 08:45:05 +0200

    Added utils.dict

---
+++ sploitkit/utils/dict.py
@@ -0,0 +1,48 @@
+from __future__ import unicode_literals
+
+from inspect import isclass
+
+from .path import Path
+
+
+__all__ = ["ClassRegistry", "PathBasedDict"]
+
+
+class ClassRegistry(dict):
+    """ Custom dictionary class using class-based keys and list values. """
+    def key(self, name):
+        """ Get class-based key from its name. """
+        for k in self.keys():
+            if isclass(k) and k.__name__.lower() == name.lower():
+                return k
+    
+    def value(self, key, name):
+        """ Get class-based value from a list associated to a given key. """
+        if not isclass(key):
+            key = self.key(key)
+        if key is not None:
+            for v in self[key]:
+                if isclass(v) and v.__name__.lower() == name.lower():
+                    return v
+
+
+class PathBasedDict(dict):
+    """ Enhanced dictionary class. """
+    def rcount(self, path=None, **kwargs):
+        """ Count the number of leaf values under the given path of keys. """
+        def _rcount(d=self, a=0):
+            for k, v in d.items():
+                a += (1 if len(kwargs) == 0 or \
+                      all(getattr(v, attr, value) == value \
+                      for attr, value in kwargs.items()) else 0) \
+                     if not isinstance(v, dict) else _rcount(v)
+            return a
+        return _rcount(self.rget(path))
+
+    def rget(self, path=None):
+        """ Find the subdictionary matching the given path of keys. """
+        def _rget(p=path, d=self):
+            p = Path(str(p))
+            _ = p.parts
+            return _rget(p.child, d[_[0]]) if len(_) > 1 else d[_[0]]
+        return self if path is None else _rget()
