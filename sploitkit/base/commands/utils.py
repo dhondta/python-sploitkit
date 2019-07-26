a78cfd8fe6ee2e2c426ef456be8a62d2580cce33
dhondta <alexandre.dhondt@gmail.com>
2019-07-26 11:05:03 +0200

    Added Memory in base.commands

--- sploitkit/base/commands/utils.py
+++ sploitkit/base/commands/utils.py
@@ -1,14 +1,65 @@
-from __future__ import unicode_literals
-
+# -*- coding: UTF-8 -*-
 import os
 import stat
+from gc import get_objects, get_referrers
+from sys import getrefcount
 
-from sploitkit import Command
+from sploitkit import *
 
 
 # ---------------------------- GENERAL-PURPOSE COMMANDS ------------------------
+class Memory(Command):
+    """ Inspect memory consumption """
+    keys         = ["graph", "growth", "info", "leaking", "objects", "refs"]
+    requirements = {
+        'python': ["objgraph", "psutil", "xdot"],
+        'system': ["xdot"],
+    }
+    
+    def complete_values(self, key=None):
+        if key in ["graph", "refs"]:
+            return [str(o) for o in get_objects() if isinstance(o, Console)]
+    
+    def run(self, key, value=None):
+        if value is not None:
+            obj = list(filter(lambda o: str(o) == value, get_objects()))[0]
+        if key == "graph":
+            from objgraph import show_refs
+            if value is None:
+                show_refs(self.console if self.console.parent is None else \
+                          self.console.parent, refcounts=True, max_depth=3)
+            else:
+                show_refs(obj, refcounts=True, max_depth=3)
+        elif key == "growth":
+            from objgraph import get_leaking_objects, show_most_common_types
+            show_most_common_types(objects=get_leaking_objects())
+        elif key == "info":
+            from psutil import Process
+            p = Process(os.getpid())
+            print(p.memory_info())
+        elif key == "leaking":
+            from objgraph import get_leaking_objects
+            print(get_leaking_objects())
+        elif key == "objects":
+            for o in get_objects():
+                if isinstance(o, (Console, Module)):
+                    print(o)
+        elif key == "refs":
+            if value is not None:
+                print(getrefcount(obj), ":", get_referrers(obj))
+    
+    def validate(self, key, value=None):
+        if key in ["graph", "refs"]:
+            if value and value not in self.complete_values("graph"):
+                raise ValueError("bad object")
+        elif value:
+            raise ValueError("this key takes no value")
+
+
 class Pydbg(Command):
     """ Start a Python debugger session """
+    requirements = {'python': ["pdb"]}
+
     def run(self):
         import pdb
         pdb.set_trace()
