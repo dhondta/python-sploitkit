21f4447a027fc7fd5330a6a4e573b869c52ebe41
dhondta <alexandre.dhondt@gmail.com>
2019-07-26 11:03:34 +0200

    Moved tokenization from core.components to core.console

--- sploitkit/core/components/completer.py
+++ sploitkit/core/components/completer.py
@@ -1,5 +1,4 @@
-from __future__ import unicode_literals
-
+# -*- coding: UTF-8 -*-
 import shlex
 from prompt_toolkit.completion import Completer, Completion
 
@@ -11,25 +10,15 @@ slist   = lambda l:    sorted(l, key=lambda s: s.casefold())
 wfilter = lambda l, w: [x for x in l if w is None or x.startswith(w)]
 
 
-def get_tokens(text, suffix=("", "\"", "'")):
-    """ Recursive token split function handling ' and ". """
-    try:
-        return shlex.split(text + suffix[0])
-    except ValueError:
-        return get_tokens(text, suffix[1:])
-    except IndexError:
-        return []
-
-
 class CommandCompleter(Completer):
     """ Completer for console's commands and arguments. """
     def get_completions(self, document, complete_event):
         # this completion method handles the following formats:
-        #  1) COMMAND VALUE        ; e.g. create my-project
-        #  2) COMMAND OPTION VALUE ; e.g. set LHOST 192.168.1.1
+        #  1) COMMAND VALUE     ; e.g. create my-project
+        #  2) COMMAND KEY VALUE ; e.g. set LHOST 192.168.1.1
         # first, tokenize document.text and initialize some shorcut variables
         _ = document.text
-        tokens = get_tokens(_)
+        tokens = self.console._get_tokens(_)
         l = len(tokens)
         ts = len(_) - len(_.rstrip(" "))  # trailing spaces
         try:
@@ -40,7 +29,7 @@ class CommandCompleter(Completer):
         it = len(_) - bc > 0
         o1 = len(cmd) + 1 - bc if cmd else 0
         o2 = len(cmd) + len(t1 or "") + 2 - bc if cmd and t2 else 0
-        _ = self.console.commands
+        _ = {k: v for k, v in self.console.commands.items()}
         c = _[cmd]() if cmd in _ else None
         cmds = slist(_.keys())
         nargs = len(c.args) if c is not None else 0
@@ -56,37 +45,37 @@ class CommandCompleter(Completer):
             if ts == 0 and c not in cmds:
                 for _ in wfilter(cmds, cmd):
                     yield Completion(_, start_position=-bc)
-            # when a valid command is provided, yield the list of valid options
+            # when a valid command is provided, yield the list of valid keys
             #  or values, depending on the type of command
             elif ts > 0 and c is not None:
                 if nargs == 1:    # COMMAND VALUE
                     for _ in slist(c.complete_values() or []):
                         yield Completion(_, start_position=0)
                 # e.g.  set  ---> ["WORKSPACE", ...]
-                elif nargs == 2:  # COMMAND OPTION VALUE
-                    for _ in slist(c.complete_options() or []):
+                elif nargs == 2:  # COMMAND KEY VALUE
+                    for _ in slist(c.complete_keys() or []):
                         yield Completion(_, start_position=0)
         # when two tokens are provided, handle format:
-        #   COMMAND [PARTIAL_](OPTION ...|VALUE)
+        #   COMMAND [PARTIAL_](KEY ...|VALUE)
         elif l == 2 and c is not None:
             # when a partial value token is given, yield the list of valid ones
             # e.g.  select my-pro  ---> ["my-project", ...]
             if nargs == 1 and ts == 0:
                 for _ in wfilter(slist(c.complete_values() or []), t1):
                     yield Completion(_, start_position=o1)
-            # when a partial option token is given, yield the list of valid ones
+            # when a partial key token is given, yield the list of valid ones
             # e.g.  set W  ---> ["WORKSPACE"]
             elif nargs == 2 and ts == 0:
-                for _ in wfilter(slist(c.complete_options() or []), t1):
+                for _ in wfilter(slist(c.complete_keys() or []), t1):
                     yield Completion(_, start_position=o1)
-            # when a valid option token is given, yield the list of values
+            # when a valid key token is given, yield the list of values
             # e.g.  set WORKSPACE  ---> ["/home/user/...", "..."]
-            elif nargs == 2 and ts > 0 and t1 in c.options:
+            elif nargs == 2 and ts > 0 and t1 in c.complete_keys():
                 for _ in slist(c.complete_values(t1) or []):
                     yield Completion(_, start_position=0)
         # when three tokens are provided, handle format:
-        #   COMMAND OPTION [PARTIAL_]VALUE
-        elif l == 3 and c is not None and t1 in c.options:
+        #   COMMAND KEY [PARTIAL_]VALUE
+        elif l == 3 and c is not None and t1 in c.complete_keys():
             if nargs == 2 and ts == 0:
                 for _ in wfilter(slist(c.complete_values(t1) or []), t2):
                     yield Completion(_, start_position=o2)
--- sploitkit/core/components/validator.py
+++ sploitkit/core/components/validator.py
@@ -1,5 +1,4 @@
-from __future__ import unicode_literals
-
+# -*- coding: UTF-8 -*-
 import shlex
 from inspect import getargspec
 from prompt_toolkit.validation import Validator, ValidationError
@@ -8,21 +7,11 @@ from prompt_toolkit.validation import Validator, ValidationError
 __all__ = ["CommandValidator"]
 
 
-def get_tokens(text, suffix=("", "\"", "'")):
-    """ Recursive token split function handling ' and ". """
-    try:
-        return shlex.split(text + suffix[0])
-    except ValueError:
-        return get_tokens(text, suffix[1:])
-    except IndexError:
-        return []
-
-
 class CommandValidator(Validator):
     """ Completer for console's commands and arguments. """
     def validate(self, document):
         # first, tokenize document.text
-        tokens = get_tokens(document.text.strip())
+        tokens = self.console._get_tokens(document.text.strip())
         l = len(tokens)
         # then handle tokens
         commands = self.console.commands
@@ -32,7 +21,7 @@ class CommandValidator(Validator):
         # when a command is being typed, mention if it is existing
         cmd = tokens[0]
         if l == 1 and cmd not in commands.keys():
-            raise ValidationError(message="Non-existent command")
+            raise ValidationError(message="Unknown command")
         # when a valid first token is provided, handle command's validation, if
         #  any available
         elif l >= 1 and cmd in commands.keys():
@@ -42,7 +31,7 @@ class CommandValidator(Validator):
             except Exception as e:
                 m = "Command syntax: %s{}" % c.signature.format(cmd)
                 e = str(e)
-                if not str(e).startswith("validate() "):
+                if not e.startswith("validate() "):
                     m = m.format([" (" + e + ")", ""][len(e) == 0])
                 else:
                     m = m.format("")
