45260f18cc75cf91eeac9678446828a02d7541b4
dhondta <alexandre.dhondt@gmail.com>
2019-09-08 21:23:56 +0200

    Added utils.dict.ExpiringDict

--- sploitkit/utils/dict.py
+++ sploitkit/utils/dict.py
@@ -1,10 +1,12 @@
 # -*- coding: UTF-8 -*-
+from collections import OrderedDict
 from inspect import isclass
+from time import time
 
 from .path import Path
 
 
-__all__ = ["ClassRegistry", "PathBasedDict"]
+__all__ = ["ClassRegistry", "ExpiringDict", "PathBasedDict"]
 
 
 class ClassRegistry(dict):
@@ -29,6 +31,61 @@ class ClassRegistry(dict):
             for v in self[key]:
                 if isclass(v) and v.__name__.lower() == name.lower():
                     return v
+
+
+class ExpiringDict(OrderedDict):
+    """ Dictionary class with expiring keys, keeping these in chronological 
+         order (regarding arrival or refresh if enabled). """
+    def __init__(self, items=None, max_age=None, refresh=True, **kwargs):
+        self.__locked = False
+        self.__times = {}
+        self.max_age = max_age
+        self.refresh = refresh
+        if isinstance(items, dict):
+            for k, v in sorted(items.items(), key=lambda x: x[0]):
+                self[k] = v
+        for k, v in kwargs.items():
+            self[k] = v
+    
+    def __check_expiration(self, key):
+        t = self.__times.get(key)
+        if t is not None and time() - t > self.max_age:
+            del self[key]
+            return True
+        return False
+    
+    def __cleanitems__(self):
+        if self.__locked:
+            return
+        # remove heading expired items
+        for key in list(self.keys()):
+            if not self.__check_expiration(key):
+                break
+    
+    def __delitem__(self, key):
+        super(ExpiringDict, self).__delitem__(key)
+        del self.__times[key]
+    
+    def __getattribute__(self, name):
+        if not name.startswith("_ExpiringDict__") and not self.__locked and \
+           not getattr(self, "_ExpiringDict__cleaned", False):
+            self.__cleaned = True
+            self.__cleanitems__()
+            self.__cleaned = False
+        return super(ExpiringDict, self).__getattribute__(name)
+    
+    def __setitem__(self, key, value):
+        # keep new keys at the tail of the dictionary
+        if self.get(key) is not None:
+            del self[key]
+        super(ExpiringDict, self).__setitem__(key, value)
+        self.__times[key] = time()
+    
+    def lock(self):
+        self.__locked = True
+    
+    def unlock(self):
+        self.__locked = False
 
 
 class PathBasedDict(dict):
