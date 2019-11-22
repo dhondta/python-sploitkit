c7179fde8de1ee87360b75271a873311cbb25e1c
YouthfulIndiscretion <55487223+YouthfulIndiscretion@users.noreply.github.com>
2019-11-22 09:15:03 +0100

    update `inspect.getargspec`
    `inspect.getargspec` has been deprecated since 3.0.
    
    Ref: https://docs.python.org/3.8/library/inspect.html?highlight=inspect%20argspec#inspect.getargspec

--- sploitkit/core/command.py
+++ sploitkit/core/command.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 import gc
 import re
-from inspect import getargspec
+from inspect import getfullargspec
 
 from .components.config import Config
 from .entity import Entity, MetaEntity
@@ -40,7 +40,7 @@ class MetaCommand(MetaEntity):
     style = "slugified"
     
     def __init__(self, *args):
-        argspec = getargspec(self.run)
+        argspec = getfullargspec(self.run)
         s, args, defs = "{}", argspec.args[1:], argspec.defaults
         for a in args[:len(args)-len(defs or [])]:
             s += " " + a
