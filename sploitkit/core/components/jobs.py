c9dfa81482bfde37a51bbd18e0714d8fee439c60
dhondta <alexandre.dhondt@gmail.com>
2019-12-02 13:25:08 +0100

    Improved jobs manager

--- sploitkit/core/components/jobs.py
+++ sploitkit/core/components/jobs.py
@@ -1,8 +1,8 @@
 # -*- coding: UTF-8 -*-
 import shlex
+import subprocess
 from prompt_toolkit.patch_stdout import patch_stdout
 from six import string_types
-from subprocess import call, Popen, TimeoutExpired, PIPE, STDOUT
 from time import time
 
 from sploitkit.core.components.logger import null_logger
@@ -14,36 +14,46 @@ communicate = lambda p, **i: tuple(map(lambda x: x.decode().strip(),
                                        p.communicate(**i)))
 
 
+class Job(subprocess.Popen):
+    def __init__(self, cmd, **kwargs):
+        self.parent = kwargs.pop('parent')
+        if not kwargs.pop('no_debug', False):
+            c = " ".join(cmd) if isinstance(cmd, (tuple, list)) else cmd
+            self.parent.logger.debug(c)
+        cmd = shlex.split(cmd) if isinstance(cmd, string_types) else cmd
+        super(Job, self).__init__(cmd, stdout=subprocess.PIPE, **kwargs)
+    
+    def close(self, wait=True):
+        for s in ["stdin", "stdout", "stderr"]:
+            getattr(getattr(self, s, object()), "close", lambda: None)()
+        if wait:
+            return self.wait()
+
+
 class JobsPool(object):
     def __init__(self, max_jobs=None):
-        self.__jobs = []
+        self.__jobs = {None: []}
         self.max = max_jobs
     
     def background(self, cmd, **kwargs):
-        p = self.process(cmd, **kwargs)
-        self.__jobs.append(p)
+        subpool = kwargs.pop('subpool')
+        self.__jobs.setdefault(subpool, [])
+        self.__jobs[subpool].append(Job(cmd, parent=self, **kwargs))
     
     def call(self, cmd, **kwargs):
-        kwargs['stdout'], kwargs['stderr'] = PIPE, PIPE
-        return call(shlex.split(cmd), **kwargs)
+        kwargs['stdout'] = kwargs['stderr'] = subprocess.PIPE
+        return subprocess.call(shlex.split(cmd), **kwargs)
     
-    def free(self):
-        for p in self.__jobs:
+    def free(self, subpool=None):
+        for p in self.__jobs[subpool]:
             if p.poll():
-                self.__jobs.remove(p)
-    
-    def process(self, cmd, **kwargs):
-        if not kwargs.pop('no_debug', False):
-            c = " ".join(cmd) if isinstance(cmd, (tuple, list)) else cmd
-            self.logger.debug(c)
-        cmd = shlex.split(cmd) if isinstance(cmd, string_types) else cmd
-        p = Popen(cmd, stdout=PIPE, **kwargs)
-        return p
+                p.close(False)
+                self.__jobs[subpool].remove(p)
     
     def run(self, cmd, stdin=None, show=False, timeout=None, **kwargs):
-        kwargs['stderr'] = PIPE
-        kwargs['stdin'] = (None if stdin is None else PIPE)
-        p = self.process(cmd, **kwargs)
+        kwargs['stderr'] = subprocess.PIPE
+        kwargs['stdin'] = (None if stdin is None else subprocess.PIPE)
+        p = Job(cmd, parent=self, **kwargs)
         com_kw = {}
         if stdin is not None:
             com_kw['input'] = stdin.encode()
@@ -53,15 +63,15 @@ class JobsPool(object):
         try:
             out, err = tuple(map(lambda x: x.decode().strip(),
                                  p.communicate(**com_kw)))
-        except (KeyboardInterrupt, TimeoutExpired):
-            _ = []
+        except (KeyboardInterrupt, subprocess.TimeoutExpired):
+            out = []
             for line in iter(p.stdout.readline, ""):
-                _.append(line)
-            out = "\n".join(_)
-            _ = []
+                out.append(line)
+            out = "\n".join(out)
+            err = []
             for line in iter(p.stderr.readline, ""):
-                _.append(line)
-            err = "\n".join(_)
+                err.append(line)
+            err = "\n".join(err)
         if show:
             if out != "":
                 self.logger.info(out)
@@ -70,24 +80,25 @@ class JobsPool(object):
         return out, err
     
     def run_iter(self, cmd, timeout=None, **kwargs):
-        kwargs['stderr'] = STDOUT
+        kwargs['stderr'] = subprocess.STDOUT
         kwargs['universal_newlines'] = True
-        p = self.process(cmd, **kwargs)
+        p = Job(cmd, parent=self, **kwargs)
         s = time()
-        for line in iter(p.stdout.readline, ""):
-            if len(line) > 0:
-                yield line
-            if timeout is not None and time() - s > timeout:
-                break
         try:
+            for line in iter(p.stdout.readline, ""):
+                if len(line) > 0:
+                    yield line
+                if timeout is not None and time() - s > timeout:
+                    break
+        finally:
             p.kill()
-        except:
-            pass
+            p.close()
     
-    def terminate(self):
-        for p in self.__jobs:
+    def terminate(self, subpool=None):
+        for p in self.__jobs[subpool]:
             p.terminate()
-            self.__jobs.remove(p)
+            p.close()
+            self.__jobs[subpool].remove(p)
     
     @property
     def logger(self):
